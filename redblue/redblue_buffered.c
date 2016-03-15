f50023f293ef45420dd28a4674b931188308618d
ken <kkoh6488@uni.sydney.edu>
2016-03-15 21:04:45 +1100

    Added execution timer; split redblue procedure into separate file

--- redblue/redblue_buffered.c
+++ redblue/redblue_buffered.c
@@ -4,11 +4,11 @@
 #include <time.h>
 #include "debuggrid.h"
 #include <math.h>
+#include <time.h>
+#include "redblueprocedure.h"
 
 void board_init(int** grid, int size);
 int malloc2darray(int ***array, int x, int y);
-void solveredturn(int **subgrid, int height, int width);
-void solveblueturn(int **subgrid, int *botbuffer, int height, int width);
 void setemptycells(int **subgrid, int height, int width, int intcolor); 
 void setemptybuffercells(int *buf, int size, int color);
 int free2darray(int ***array);
@@ -35,7 +35,10 @@ int main(char argc, char** argv)
 	int **grid;								// Master grid
 	int numtoexceedc	= (int)(t * t * c + 1);				// Cells to exceed the threshold
 	int numtiles		= (n / t) * (n / t);
-
+	clock_t start, end;
+	double elapsed;	
+	
+	
 	malloc2darray(&grid, n, n);
 	
 	if (rank == 0)
@@ -44,6 +47,7 @@ int main(char argc, char** argv)
 		board_init(grid, n);		
 	}
 	
+	start = clock();
 	if (worldsize > 1)
 	{
 		// Init the local 2D array for this process
@@ -240,80 +244,12 @@ int main(char argc, char** argv)
 			curriter++;	
 		}	
 	}
+	end = clock();
+	elapsed = (double)(end - start) / CLOCKS_PER_SEC;
+	printf("Execution time: %d\n", elapsed);
 	MPI_Finalize();	
-}		
-
-void solveredturn(int **subgrid, int height, int width)
-{
-	for (int x = 0; x < height; x++)
-	{
-		for (int y = 0; y < width; y++)
-		{
-			if (subgrid[x][y] == 1)				// If this cell is red
-			{
-				if (y < width - 1)			// If this isn't the right edge cell
-				{
-					if (subgrid[x][y + 1] == 0)	// If the cell to the right is white
-					{
-						subgrid[x][y + 1] = 3;	// Mark it as just moved in
-						subgrid[x][y] = 0; 	
-					}
-				}
-				else
-				{
-					if (subgrid[x][0] == 0)		
-					{
-						subgrid[x][0] = 3;	// Mark the new cell
-						subgrid[x][y] = 4;	// Show this cell was left this turn
-					}
-				}	
-			}
-		} 
-	}	
 }
-
-
-void solveblueturn(int **subgrid, int *botbuffer, int height, int width)
-{
-	for (int x = 0; x < height; x++)
-	{
-		for (int y = 0; y < width; y++)
-		{
-			if (subgrid[x][y] == 2)
-			{
-				if (x < height - 1)			// If this isn't the bottom edge cell
-				{
-					if (subgrid[x + 1][y] == 0)	// If the cell below is white
-					{
-						subgrid[x + 1][y] = 3;
-						subgrid[x][y] = 0; 	
-					}
-				}
-				else					// This row is the bottom row of the localgrid
-				{
-					if (!botbuffer)
-					{
-						
-						if (subgrid[0][y] == 0)		// Check the top row cell for wraparound
-						{
-							subgrid[0][y] = 3;
-							subgrid[x][y] = 4;
-						}	
-					}
-					else
-					{
-						if (botbuffer[y] == 0)
-						{
-							subgrid[x][y] = 4;
-							botbuffer[y] = 3;
-						}
-					}
-				}	
-			}
-		} 
-	}
-}
-
+		
 // For each process, look at the top row and compare it with the bottom buffer of (rank - 1) process
 // If number moved in (ie is 3) in the bottom buffer, mark the new entry in the top row of this process.
 void updatetoprow(int *toprow, int *tempbuffer, int size)
