f63df402fe1bcac5e3d815c8438dde9a61c038b1
Ken <kkoh94@gmail.com>
2016-03-19 11:00:03 +1100

    Cleaned up sendrecv targets; fixed incorrect number of extra rows procs

--- redblue/redblue_buffered.c
+++ redblue/redblue_buffered.c
@@ -13,11 +13,13 @@ int malloc2darray(int ***array, int x, int y);
 
 void solveredturn(int **subgrid, int height, int width);
 
-void solveblueturn(int **subgrid, int height, int width);
+void solveblueturn(int **subgrid, int *botbuffer, int height, int width);
 
 void setemptycells(int **subgrid, int height, int width, int intcolor); 
 
 int free2darray(int ***array);
+
+void updatebotbuffer(int *botbuffer, int *tempbuffer,  int size);
 
 int main(char argc, char** argv)
 {
@@ -36,7 +38,6 @@ int main(char argc, char** argv)
 	int t 		= strtol(argv[2], NULL, 10);			// Tile size
 	float  c 	= atof(argv[3]);			// Terminating threshold
 	int max_iters 	= strtol(argv[4], NULL, 10);		// Max iterations
-	//int grid[n][n];
 	int **grid;
 	
 	malloc2darray(&grid, n, n);
@@ -52,24 +53,15 @@ int main(char argc, char** argv)
 		// Init the local 2D array for this process
 		int rowsperproc = n / worldsize;
 		int extrarowsforproc = n % worldsize;		
-		int mynumrows;		
-		int procswithextrarows = n - worldsize;		
+		int mynumrows;
+		int procswithextrarows = extrarowsforproc % worldsize;				
 
 		// Local arrays
 		int **localgrid;
 		
 		// Assign the rows for this process
 		// Split up the rows evenly
-		/*
-		if (rank == 0)
-		{
-			mynumrows = rowsperproc + extrarowsforproc;	
-		}
-		else
-		{
-			mynumrows = rowsperproc;	
-		}
-		*/
+		
 		if (rank < procswithextrarows)
 		{
 			mynumrows = rowsperproc + extrarowsforproc / procswithextrarows;
@@ -96,6 +88,7 @@ int main(char argc, char** argv)
 				
 			int dest = 1;
 			int counter = 0;
+			printf("Procs with extra rows: %d \n", procswithextrarows);
 			//Send rows from master to worker processes
 			for (int x = mynumrows; x < n; x++)
 			{
@@ -137,56 +130,45 @@ int main(char argc, char** argv)
 		solveredturn(localgrid, mynumrows, n);
 		setemptycells(localgrid, mynumrows, n,  1);
 		
-		int* topbuffer =  (int*) malloc ((n + 1) * sizeof (int));
-		int* botbuffer =  (int*) malloc ((n + 1) * sizeof (int)); 
+		int* tempbotbuffer =  (int*) malloc (n * sizeof (int));
+		int* botbuffer =  (int*) malloc (n * sizeof (int)); 
 
-		//printf("Len of botbuffer: %d \n", (n * sizeof(int)));		
-
-	
+		int src, dest;
+		
 		// For each process, get the row number it needs for the bottom buffer
 		// Each process sends its top row to the previous process bot row
 		for (int i = 0; i < worldsize; i++)
 		{
 			if (rank == i)
 			{
-				if (i == 0)
+				if (rank == 0)
 				{
-					MPI_Sendrecv(&localgrid[0][0], n, MPI_INT, worldsize - 1, 0, botbuffer, n, MPI_INT, rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-					//MPI_Send(&localgrid[0][0], n, MPI_INT, 1, 0, MPI_COMM_WORLD);
-					printf("Sent from master\n");
-					
+					dest = worldsize - 1;
+					src = rank + 1;
 				}
-				else if (i == worldsize - 1)
+				else if (rank == worldsize - 1)
 				{
-					//MPI_Recv(botbuffer, n, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-					//int startrow = mynumrows * rank + extrarowsforproc;
-					//printf("Sending last proc, starting from row %d \n", startrow);
-					printf("Sending from rank %d to %d, %d ints\n", rank, rank - 1, n);
-					MPI_Sendrecv(&localgrid[0][0], n, MPI_INT, rank - 1, 0, botbuffer, n, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-					for (int a = 0; a < n ; a++)
-					{
-						printf("%d ", botbuffer[a]);
-					}
-					printf("\n");
+					dest = rank - 1;
+					src = 0;
 				}
 				else
 				{
-					//int startrow = mynumrows * rank + extrarowsforproc;
-					MPI_Sendrecv(&localgrid[0][0], n, MPI_INT, rank - 1, 0, botbuffer, n, MPI_INT, rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+					dest = rank - 1;
+					src = rank + 1; 
 				}
+				MPI_Sendrecv(&localgrid[0][0], n, MPI_INT, dest, 0, botbuffer, n, MPI_INT, src, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 			}					
 		}	
 		printf("Received rows: proc %d \n", rank);			
-		/*
-		free(botbuffer);
-		free(topbuffer);
-		free2darray(&localgrid);
-		if (rank == 0)
+		solveblueturn(localgrid, botbuffer, mynumrows, n);
+		for (int i = 0; i < worldsize; i++)
 		{
-			free2darray(&grid);
+			if (rank == i)
+			{		
+				MPI_Sendrecv(&localgrid[0][0], n, MPI_INT, dest, 0, tempbotbuffer, n, MPI_INT, src, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			}
 		}
-		printf("Freed memory \n");
-		*/	
+		updatebotbuffer(botbuffer, tempbotbuffer,  n);	
 	}
 	else
 	{
@@ -209,15 +191,7 @@ int main(char argc, char** argv)
 			}
 		}
 	}*/
-	/*
-	// Now do blue cells
-	free2darray(&localgrid);
-	malloc2darray(&localgrid, n, subgridsize); 	
-	MPI_Scatterv(gridptr, sendcounts, displacements, subgridtypeblue, &localgrid[0][0], n * subgridsize, MPI_INT, 0, MPI_COMM_WORLD);	
-	solveblueturn(localgrid, n, subgridsize);
-	setemptycells(localgrid, n, subgridsize, 2);
-	MPI_Gatherv(&localgrid[0][0], n * subgridsize, MPI_INT, gridptr, sendcounts, displacements, subgridtypeblue, 0, MPI_COMM_WORLD);
-	*/
+
 	MPI_Finalize();
 	
 	/*
@@ -242,19 +216,19 @@ void solveredturn(int **subgrid, int height, int width)
 	{
 		for (int y = 0; y < width; y++)
 		{
-			if (subgrid[x][y] == 1)
+			if (subgrid[x][y] == 1)				// If this cell is red
 			{
-				if (y < width - 1)			// If this isn't the edge cell
+				if (y < width - 1)			// If this isn't the right edge cell
 				{
 					if (subgrid[x][y + 1] == 0)	// If the cell to the right is white
 					{
-						subgrid[x][y + 1] = 3;
-						subgrid[x][y] = 4; 	
+						subgrid[x][y + 1] = 3;	// Mark it as just moved in
+						//subgrid[x][y] = 4; 	
 					}
 				}
 				else
 				{
-					if (subgrid[x][0] == 0)
+					if (subgrid[x][0] == 0)		
 					{
 						subgrid[x][0] = 3;	// Mark the new cell
 						subgrid[x][y] = 4;	// Show this cell was left this turn
@@ -266,7 +240,7 @@ void solveredturn(int **subgrid, int height, int width)
 }
 
 
-void solveblueturn(int **subgrid, int height, int width)
+void solveblueturn(int **subgrid, int *botbuffer, int height, int width)
 {
 	for (int x = 0; x < height; x++)
 	{
@@ -274,7 +248,7 @@ void solveblueturn(int **subgrid, int height, int width)
 		{
 			if (subgrid[x][y] == 2)
 			{
-				if (x < height - 1)			// If this isn't the edge cell
+				if (x < height - 1)			// If this isn't the bottom edge cell
 				{
 					if (subgrid[x + 1][y] == 0)	// If the cell below is white
 					{
@@ -282,16 +256,36 @@ void solveblueturn(int **subgrid, int height, int width)
 						subgrid[x][y] = 4; 	
 					}
 				}
-				else
+				else					// This row is the bottom row of the localgrid
 				{
-					if (subgrid[0][y] == 0)		// Check the top row cell for wraparound
+					if (!botbuffer)
 					{
-						subgrid[0][y] = 3;
-						subgrid[x][y] = 4;
+						
+						if (subgrid[0][y] == 0)		// Check the top row cell for wraparound
+						{
+							subgrid[0][y] = 3;
+							subgrid[x][y] = 4;
+						}	
+					}
+					else
+					{
+						if (botbuffer[y] == 0)
+						{
+							subgrid[x][y] = 4;
+							botbuffer[y] = 3;
+						}
 					}
 				}	
 			}
 		} 
+	}
+}
+
+void updatebotbuffer(int *botbuffer, int *tempbuffer, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		
 	}
 }
 
